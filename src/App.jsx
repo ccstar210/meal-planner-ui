883072f9eb0ea906284d021c5d411aeed53b32c4
ccstar210 <ccstar509@gmail.com>
2023-07-17 23:17:22 -0500

    Set up inital routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Inventory from './pages/Inventory';
+import Grocery from './pages/Grocery';
+import Recipes from './pages/Recipes';
+import NavBar from './components/layouts/NavBar';
 
 function App() {
-  return <div>Meal Planner</div>;
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/grocery" element={<Grocery />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="*" element={<div>Not found</div>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
